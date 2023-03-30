d0c0ad8faf9c79a48f74a94ed080b2e4e2af77d8
Preston Mesecher <pmmesecher@dmacc.edu>
2023-03-29 20:09:07 -0500

    03.29.2023
    DataValidationMesecher

---
+++ DataValidationMesecher/DataValidationMesecher/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using FirstResponsiveWebAppMesecher.Models;
+using Microsoft.AspNetCore.Mvc;
+
+
+public class HomeController : Controller
+{
+    public const int DaysInAYear = 364;
+    [HttpGet]
+    public IActionResult Index()
+    {
+        ViewBag.Name = "";
+        ViewBag.Age = "";
+        return View();
+    }
+    [HttpPost]
+    public IActionResult Index(FirstResponsiveWebAppModel model)
+    {
+        if (ModelState.IsValid)
+        {
+            ViewBag.Name = model.Name;
+            ViewBag.Age = "Age: " + model.CalculateAge(DaysInAYear);
+        }
+        else
+        {
+            ViewBag.Name = "";
+            ViewBag.Age = "";
+        }
+        return View(model);
+    }
+}
---
+++ DataValidationMesecher/DataValidationMesecher/Models/FirstResponsiveWebAppModel.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+using System.Net.Mail;
+using System.Net.NetworkInformation;
+
+
+
+namespace FirstResponsiveWebAppMesecher.Models
+{
+    public class PastDateAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            value = (DateTime)value;
+            if (DateTime.Now.CompareTo(value) >= 0)
+            {
+                return ValidationResult.Success;
+            }
+            else
+            {
+                return new ValidationResult("Date must be in the past.");
+            }
+        }
+    }
+    public class FirstResponsiveWebAppModel
+    {
+        
+        [Required(ErrorMessage = "Please enter a name.")]
+        public String? Name { get; set; }
+
+        [PastDate]
+        public DateTime Birthdate { get; set; }
+
+        public int? CalculateAge(int DaysInAYear)
+        {
+
+            DateTime Today = DateTime.Now;
+            TimeSpan DateDiff = Today - Birthdate;
+            var Age = DateDiff.Days/DaysInAYear;
+
+            if (Birthdate > Today.AddYears(-Age))
+            Age--;
+            return Age;
+        }
+    }
+}
